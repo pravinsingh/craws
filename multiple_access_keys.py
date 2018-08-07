27b6ed3e8cdcc983bab6e956e7667befeaa4047f
Anmol Saini <asaini@MacBook-Pro.local>
2018-08-07 16:49:29 -0400

    Adding python script to get list of users having multiple access keys

---
+++ multiple_access_keys.py
@@ -0,0 +1,100 @@
+""" This rule checks users having multiple access/secret keys.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Anmol Saini'
+
+import boto3
+import craws
+import time
+import datetime
+
+
+
+def handler(event, context):
+    logger = craws.get_logger(name='MultipleAccessKeys')
+    logger.debug('Multiple Access Keys check started')
+
+    sts = boto3.client('sts')
+
+    for role_arn in craws.role_arns:
+        results = {'Rule Name': 'Multiple Access Keys'}
+        results['Area'] = 'IAM'
+        results['Description'] = 'Auditing all IAM users access/secret keys is a good way in order to secure the AWS account against' +\
+            ' attackers. This rule will keep a check on all users having multiple,' +\
+            'access/secret keys .'
+        details = []
+        try:
+            response = sts.assume_role(RoleArn=role_arn, RoleSessionName='MultipleKeys')
+        except Exception as e:
+            logger.error(e)
+            continue
+        credentials = response['Credentials']
+        # We need to get the sts client again, with the temp tokens. Otherwise any attempt to get the account id 
+        # will return the account id of the original caller and not the account id of the assumed role.
+        sts_client = boto3.client('sts', aws_access_key_id=credentials['AccessKeyId'], 
+                                    aws_secret_access_key=credentials['SecretAccessKey'], 
+                                    aws_session_token=credentials['SessionToken'])
+        account_id = sts_client.get_caller_identity().get('Account')
+        #regions = craws.get_region_descriptions()
+        #total_count = len(regions)
+        green_count = red_count = orange_count = yellow_count = grey_count = 0
+        
+        
+        iam_client = boto3.client('iam',
+                                        aws_access_key_id=credentials['AccessKeyId'], 
+                                        aws_secret_access_key=credentials['SecretAccessKey'], 
+                                        aws_session_token=credentials['SessionToken'])
+        iam = boto3.resource('iam',
+                                        aws_access_key_id=credentials['AccessKeyId'], 
+                                        aws_secret_access_key=credentials['SecretAccessKey'], 
+                                        aws_session_token=credentials['SessionToken'])
+        
+        #AccessId = None
+        
+        
+        
+        try:
+            
+            for user in iam.users.all():
+                
+                count = 0
+                
+                result = []
+                for access_key in user.access_keys.all():
+                    #AccessId = access_key.access_key_id
+                    #if AccessId is not None:
+                    count = count + 1    
+                    
+                if count > 1:
+                    #print(user.name+': ' + "has multiple access keys")
+                    details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Red']})
+                    red_count += 1
+                     
+                elif count == 1:
+                    
+                    #print(user.name+': ' + "don't have multiple keys")
+                    details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Green']})
+                    green_count += 1
+                    
+                else:
+                    details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Grey']})
+                    grey_count += 1
+                    
+    
+        
+        except Exception as e:
+            logger.error(e)
+            details.append({'Details': e, 'Status': craws.status['Grey']})
+            grey_count += 1
+
+        results['Details'] = details
+        results['GreenCount'] = green_count
+        results['RedCount'] = red_count
+        results['OrangeCount'] = orange_count
+        results['YellowCount'] = yellow_count
+        results['GreyCount'] = grey_count
+        craws.upload_result_json(results, 'MultipleAccessKeys.json', account_id)
+        logger.info('Results for accout %s uploaded to s3', account_id)
+    
+    logger.debug('Multiple Access Keys check finished')
