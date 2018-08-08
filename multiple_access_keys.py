4f10b7b27394ebec9411d8927003ca2eeb6a12d6
Anmol Saini <asaini@MacBook-Pro.local>
2018-08-08 09:42:15 -0400

    Adding python script to get list of users having multiple access keys

--- multiple_access_keys.py
+++ multiple_access_keys.py
@@ -17,27 +17,20 @@ def handler(event, context):
 
     sts = boto3.client('sts')
 
-    for role_arn in craws.role_arns:
+    for account in craws.accounts:
         results = {'Rule Name': 'Multiple Access Keys'}
         results['Area'] = 'IAM'
         results['Description'] = 'Auditing all IAM users access/secret keys is a good way in order to secure the AWS account against' +\
-            ' attackers. This rule will keep a check on all users having multiple,' +\
+            ' attackers. This rule will keep a check on all users having multiple ' +\
             'access/secret keys .'
         details = []
         try:
-            response = sts.assume_role(RoleArn=role_arn, RoleSessionName='MultipleKeys')
+            response = sts.assume_role(RoleArn=account['role_arn'], RoleSessionName='MultipleKeys')
         except Exception as e:
             logger.error(e)
             continue
         credentials = response['Credentials']
-        # We need to get the sts client again, with the temp tokens. Otherwise any attempt to get the account id 
-        # will return the account id of the original caller and not the account id of the assumed role.
-        sts_client = boto3.client('sts', aws_access_key_id=credentials['AccessKeyId'], 
-                                    aws_secret_access_key=credentials['SecretAccessKey'], 
-                                    aws_session_token=credentials['SessionToken'])
-        account_id = sts_client.get_caller_identity().get('Account')
-        #regions = craws.get_region_descriptions()
-        #total_count = len(regions)
+        regions = craws.get_region_descriptions()
         green_count = red_count = orange_count = yellow_count = grey_count = 0
         
         
@@ -71,21 +64,22 @@ def handler(event, context):
                     details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Red']})
                     red_count += 1
                      
-                elif count == 1:
+                #elif count == 1:
+                else:
                     
                     #print(user.name+': ' + "don't have multiple keys")
                     details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Green']})
                     green_count += 1
                     
-                else:
-                    details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Grey']})
-                    grey_count += 1
+                #else:
+                #    details.append({'User Name':user.name,'ARN': user.arn, 'Status': craws.status['Grey']})
+                #    grey_count += 1
                     
     
         
         except Exception as e:
             logger.error(e)
-            details.append({'Details': e, 'Status': craws.status['Grey']})
+            details.append({'Details': e,'ARN': user.arn, 'Status': craws.status['Grey']})
             grey_count += 1
 
         results['Details'] = details
@@ -94,7 +88,7 @@ def handler(event, context):
         results['OrangeCount'] = orange_count
         results['YellowCount'] = yellow_count
         results['GreyCount'] = grey_count
-        craws.upload_result_json(results, 'MultipleAccessKeys.json', account_id)
-        logger.info('Results for accout %s uploaded to s3', account_id)
+        craws.upload_result_json(results, 'MultipleAccessKeys.json', account['account_id'])
+        logger.info('Results for accout %s uploaded to s3', account['account_id'])
     
     logger.debug('Multiple Access Keys check finished')
