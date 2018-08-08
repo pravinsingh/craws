650061269bb9ee84c8fe6fc8f2b267296d150cfe
Anmol Saini <asaini@MacBook-Pro.local>
2018-08-08 10:07:44 -0400

    Modify python script for rds with disabled automated backup

--- rds_disabled_backup.py
+++ rds_disabled_backup.py
@@ -14,23 +14,19 @@ def handler(event, context):
 
     sts = boto3.client('sts')
 
-    for role_arn in craws.role_arns:
+    for account in craws.accounts:
         results = {'Rule Name': 'RDS Instances with Disabled Automated Backup'}
+        results['Area'] = 'RDS'
+        results['Description'] = 'Ensure that no AWS RDS database instances has automated backup '  +\
+            'disabled .'
         details = []
         try:
-            response = sts.assume_role(RoleArn=role_arn, RoleSessionName='RdsWithNoBackup')
+            response = sts.assume_role(RoleArn=account['role_arn'], RoleSessionName='RdsWithNoBackup')
         except Exception as e:
             logger.error(e)
             continue
         credentials = response['Credentials']
-        # We need to get the sts client again, with the temp tokens. Otherwise any attempt to get the account id 
-        # will return the account id of the original caller and not the account id of the assumed role.
-        sts_client = boto3.client('sts', aws_access_key_id=credentials['AccessKeyId'], 
-                                    aws_secret_access_key=credentials['SecretAccessKey'], 
-                                    aws_session_token=credentials['SessionToken'])
-        account_id = sts_client.get_caller_identity().get('Account')
         regions = craws.get_region_descriptions()
-        total_count = len(regions)
         green_count = red_count = orange_count = yellow_count = grey_count = 0
 
         for region in regions:
@@ -61,14 +57,13 @@ def handler(event, context):
                 red_count += 1
 
         results['Details'] = details
-        results['TotalCount'] = total_count
         results['GreenCount'] = green_count
         results['RedCount'] = red_count
         results['OrangeCount'] = orange_count
         results['YellowCount'] = yellow_count
         results['GreyCount'] = grey_count
-        craws.upload_result_json(results, 'RdsWithDisabledBackup.json', account_id)
-        logger.info('Results for accout %s uploaded to s3', account_id)
+        craws.upload_result_json(results, 'RdsWithDisabledBackup.json', account['account_id'])
+        logger.info('Results for accout %s uploaded to s3', account['account_id'])
 
     logger.debug('Rds With Disabled Backup check finished')
 
