103465e204be7cd928e63ffffd6f283dca37d57a
Anmol Saini <asaini@MacBook-Pro.local>
2018-08-09 10:28:25 -0400

    Removed unneccessery spaces,comments from the script

--- access_keys_not_rotated.py
+++ access_keys_not_rotated.py
@@ -9,8 +9,6 @@ import craws
 import time
 import datetime
 
-
-
 def handler(event, context):
     logger = craws.get_logger(name='KeysNotRotated')
     logger.debug('Keys Not Rotated check started')
@@ -20,9 +18,10 @@ def handler(event, context):
     for account in craws.accounts:
         results = {'Rule Name': 'Access Keys Not Rotated'}
         results['Area'] = 'IAM'
-        results['Description'] = 'Auditing all IAM users access/secret keys is a good way to secure the AWS account against' +\
-            ' attackers. This rule will keep a check that all users rotate their ' +\
-            'access/secret keys monthly.'
+        results[
+            'Description'] = 'Auditing all IAM users access/secret keys is a good way to secure the AWS account against' + \
+                             ' attackers. This rule will keep a check that all users rotate their ' + \
+                             'access/secret keys monthly.'
         details = []
         try:
             response = sts.assume_role(RoleArn=account['role_arn'], RoleSessionName='KeysNotRotated')
@@ -31,52 +30,52 @@ def handler(event, context):
             continue
         credentials = response['Credentials']
         regions = craws.get_region_descriptions()
-        green_count = red_count = orange_count = yellow_count = grey_count = 0       
+        green_count = red_count = orange_count = yellow_count = grey_count = 0
 
         iam_client = boto3.client('iam',
-                                        aws_access_key_id=credentials['AccessKeyId'], 
-                                        aws_secret_access_key=credentials['SecretAccessKey'], 
-                                        aws_session_token=credentials['SessionToken'])
+                                  aws_access_key_id=credentials['AccessKeyId'],
+                                  aws_secret_access_key=credentials['SecretAccessKey'],
+                                  aws_session_token=credentials['SessionToken'])
         iam = boto3.resource('iam',
-                                        aws_access_key_id=credentials['AccessKeyId'], 
-                                        aws_secret_access_key=credentials['SecretAccessKey'], 
-                                        aws_session_token=credentials['SessionToken'])                 
-        AccessId = None        
-        
-        try:
-            for user in iam.users.all():
+                             aws_access_key_id=credentials['AccessKeyId'],
+                             aws_secret_access_key=credentials['SecretAccessKey'],
+                             aws_session_token=credentials['SessionToken'])
+        AccessId = None
+
+        for user in iam.users.all():
+            try:
                 count = 0
-                key_present = 0
-                try:
-                    result = []
+                result = []
+                if iam_client.get_login_profile(UserName=user.name):
+                    
                     for access_key in user.access_keys.all():
-                        AccessId = access_key.access_key_id
-                        key_present = key_present + 1
-                        if iam_client.get_login_profile(UserName=user.name):
-                            created_date = access_key.create_date.date()
-                            timeLimit2 = datetime.datetime.now() - datetime.timedelta(days=30)
-                            one_month_before = timeLimit2.date()
-                            if created_date < one_month_before:
-                                count = count + 1
-                    
+                        try:
+                                AccessId = access_key.access_key_id
+                                created_date = access_key.create_date.date()
+                                timeLimit2 = datetime.datetime.now() - datetime.timedelta(days=30)
+                                one_month_before = timeLimit2.date()
+                                if created_date < one_month_before:
+                                    count = count + 1
+
+                        except Exception as e:
+                            logger.error(e)
+                        #   Exception occured, mark it as Grey (not checked)
+                            details.append({'User Name': user.name, 'ARN': user.arn, 'Status': craws.status['Grey']})
+                            grey_count += 1
+
+
                     if count >= 1:
-                        details.append({'User Name': user.name,'ARN': user.arn,'Status': craws.status['Red']})
+                        details.append({'User Name': user.name, 'ARN': user.arn, 'Status': craws.status['Red']})
                         red_count += 1
-                        
+
                     else:
-                        details.append({'User Name':user.name,'ARN': user.arn,'Status': craws.status['Green']})
+                        details.append({'User Name': user.name, 'ARN': user.arn, 'Status': craws.status['Green']})
                         green_count += 1
-                    
-                except Exception as e:
-                    logger.error(e)
-                    # Exception occured, mark it as Grey (not checked)
-                    details.append({'User Name': user.name,'ARN': user.arn, 'Status': craws.status['Grey']})
-                    grey_count += 1
-                
-        except Exception as e:
-            logger.error(e)
-            details.append({'Details': e, 'Status': craws.status['Grey']})
-            grey_count += 1
+
+            except Exception as e:
+                logger.error(e)
+                details.append({'User Name': user.name, 'ARN': user.arn, 'Status': craws.status['Grey']})
+                grey_count += 1
 
         results['Details'] = details
         results['GreenCount'] = green_count
@@ -86,5 +85,5 @@ def handler(event, context):
         results['GreyCount'] = grey_count
         craws.upload_result_json(results, 'KeysNotRotated.json', account['account_id'])
         logger.info('Results for accout %s uploaded to s3', account['account_id'])
-    
+
     logger.debug('Keys Not Rotated check finished')
